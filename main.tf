ff1629f11a0123cf50e97ed4f82971b1940f2e61
“papis1984” <nkengfac@ualberta.ca>
2023-03-10 22:13:39 -0700

    added provisioner

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ variable "env_prefix" {}
 variable "my_ip" {}
 variable "instance_type" {}
 variable "my_public_ip" {}
+variable "my_private_ip" {}
 
 
 data "aws_ami" "amazon-linux-image" {
@@ -128,13 +129,6 @@ resource "aws_instance" "myapp-server" {
     Name = "${var.env_prefix}-server"
   }
 
-  user_data = <<EOF
-                 #!/bin/bash
-                 sudo yum update -y && sudo yum install -y docker
-                 sudo systemctl start docker
-                 sudo usermod -aG docker ec2-user
-                 docker run -p 8080:8080 nginx
-              EOF
 }
 
 resource "aws_instance" "myapp-server-two" {
@@ -150,11 +144,28 @@ resource "aws_instance" "myapp-server-two" {
     Name = "${var.env_prefix}-server-two"
   }
 
-  user_data = <<EOF
-                 #!/bin/bash
-                 sudo yum update -y && sudo yum install -y docker
-                 sudo systemctl start docker
-                 sudo usermod -aG docker ec2-user
-                 docker run -p 8080:8080 nginx
-              EOF
+  # user_data = file("entry-script.sh")
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = var.my_private_ip
+  }
+
+  provisioner "file" {
+    source      = "entry-script.sh"
+    destination = "/home/ec2-user/entry-script-on-ec2.sh"
+  }
+
+  provisioner "remote-exec" {
+    script = file("entry-script.sh")
+  }
+
+  provisioner "local-exec" {
+    command = "echo ${self.public_ip} > output.txt"
+  }
+
+
+
 }
